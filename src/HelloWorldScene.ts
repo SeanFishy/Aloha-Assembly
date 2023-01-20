915a1c00728148874ef9a5f9b502e3877d0ad2b9
Sean Williams <sean475@gmail.com>
2023-01-20 13:16:48 -1000

    Setup Initial Conveyor

--- src/HelloWorldScene.ts
+++ src/HelloWorldScene.ts
@@ -2,19 +2,26 @@ import Phaser from 'phaser'
 
 export default class HelloWorldScene extends Phaser.Scene {
 
+	private conveyor?: Phaser.GameObjects.TileSprite;
+
 	constructor() {
 		super('hello-world')
 	}
 
 	preload() {
-		
+		this.load.image("conveyor", "public/assets/Conveyor.png");
+		this.load.image('sky', 'assets/sky.png')
 	}
 
 	create() {
-		
+		this.add.image(400,300,'sky')
+		this.conveyor = this.add.tileSprite(30, 30, 60, 100, "conveyor");
+    	this.conveyor.setOrigin(0,0);
 	}
 
 	update() {
-		
+		if(this.conveyor){
+			this.conveyor.tilePositionX -= 4;
+		}
 	}
 }
