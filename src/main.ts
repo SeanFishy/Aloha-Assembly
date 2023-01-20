498125b57314a012b09b63837feacd11dde5182f
Maddie191891 <mgholt@udel.edu>
2023-01-20 13:35:35 -1000

    first title scene

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+//import HelloWorldScene from './HelloWorldScene'
+import IntroScene from './scenes/IntroScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
@@ -13,7 +14,7 @@ const config: Phaser.Types.Core.GameConfig = {
 			gravity: { y: 300 },
 		},
 	},
-	scene: [HelloWorldScene],
+	scene: [IntroScene],
 }
 
 export default new Phaser.Game(config)
---
+++ src/scenes/HelloWorldScene.ts
@@ -0,0 +1,20 @@
+import Phaser from 'phaser'
+
+export default class HelloWorldScene extends Phaser.Scene {
+
+	constructor() {
+		super('hello-world')
+	}
+
+	preload() {
+		
+	}
+
+	create() {
+		
+	}
+
+	update() {
+		
+	}
+}
