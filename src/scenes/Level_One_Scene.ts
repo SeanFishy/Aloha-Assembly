bcc3645829c3c03a4dbf145ae995052e82586ec3
Sean Williams <sean475@gmail.com>
2023-01-26 10:41:07 -1000

    Added pause/resume button

--- src/scenes/Level_One_Scene.ts
+++ src/scenes/Level_One_Scene.ts
@@ -38,6 +38,9 @@ export default class Level_One_Scene extends Phaser.Scene {
 
 	private aboutButton?: Phaser.GameObjects.Image;
 
+	private pauseButton?: Phaser.GameObjects.Image;
+	private resumeButton?: Phaser.GameObjects.Image;
+
 	constructor() {
 		super('level-1')
 	}
@@ -60,6 +63,8 @@ export default class Level_One_Scene extends Phaser.Scene {
 		this.load.image('close-button', 'public/assets/close.png');
 		this.load.image('fruit-intro', 'assets/emptyDescription.png');
 		this.load.image('about-button', 'assets/aboutButton.png');
+		this.load.image('pause-button', 'assets/PauseButton.png');
+		this.load.image('resume-button', 'assets/ResumeButton.png');
 	}
 
 	create() {
@@ -128,7 +133,9 @@ export default class Level_One_Scene extends Phaser.Scene {
             	this.fruitIntro.visible = false
 				this.closeButton.visible = false
 			}
-			this.infoBox = false
+			if(this.resumeButton && this.resumeButton.visible === false){
+				this.infoBox = false
+			}
         });
 
 		this.speedText = this.add.text(700, 70, 'Speed: '+this.speed, {
@@ -167,6 +174,44 @@ export default class Level_One_Scene extends Phaser.Scene {
 			}
 			this.infoBox = true
         });
+
+		this.pauseButton = this.add.image(250,35, 'pause-button').setAlpha(1);
+        this.pauseButton.setScale(.3)
+        this.pauseButton.setInteractive();
+        this.pauseButton.on("pointerover",() =>{
+            this.pauseButton?.setAlpha(.5);
+        });
+        this.pauseButton.on("pointerout", ()=>{
+            this.pauseButton?.setAlpha(1);
+        });
+        this.pauseButton.on("pointerup",()=>{
+			if(this.fruitIntro?.visible === false){
+				if(this.pauseButton && this.resumeButton){
+					this.pauseButton.visible = false;
+					this.resumeButton.visible = true;
+				}
+				this.infoBox = !this.infoBox;
+			}
+        });
+		this.resumeButton = this.add.image(250,35, 'resume-button').setAlpha(1);
+        this.resumeButton.setScale(0.05)
+        this.resumeButton.setInteractive();
+        this.resumeButton.on("pointerover",() =>{
+            this.resumeButton?.setAlpha(.5);
+        });
+        this.resumeButton.on("pointerout", ()=>{
+            this.resumeButton?.setAlpha(1);
+        });
+        this.resumeButton.on("pointerup",()=>{
+			if(this.fruitIntro?.visible === false){
+				if(this.pauseButton && this.resumeButton){
+					this.pauseButton.visible = true;
+					this.resumeButton.visible = false;
+				}
+				this.infoBox = !this.infoBox;
+			}
+        });
+		this.resumeButton.visible = false;
 	}
 
 	update() {
