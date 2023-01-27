dbb327e2314b7cdf72330ad2aacace424f0be02f
MattUDCompSci <mattweis@udel.edu>
2023-01-27 03:50:37 -0500

    Made Level 2 Functional, added all buttons and scores from level 1

--- src/scenes/Level_Two_Scene.ts
+++ src/scenes/Level_Two_Scene.ts
@@ -2,84 +2,277 @@ import Phaser from 'phaser'
 
 export default class Level_One_Scene extends Phaser.Scene {
 
+	private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
+	private conv_vertical1?: Phaser.GameObjects.TileSprite;
+	private conv_vertical2?: Phaser.GameObjects.TileSprite;
+	private conv_vertical3?: Phaser.GameObjects.TileSprite;
+	private conv_vertical4?: Phaser.GameObjects.TileSprite;
+	private conv_vertical5?: Phaser.GameObjects.TileSprite;
+	private conv_horizontal1?: Phaser.GameObjects.TileSprite;
+	private conv_horizontal2?: Phaser.GameObjects.TileSprite;
+	private arrow1?: Phaser.GameObjects.Image;
+	private arrow2?: Phaser.GameObjects.Image;
+
+	private currDirection: number = 0;
+	private speed: number = 1
+	private speedText?: Phaser.GameObjects.Text
+
+	private scoreText?: Phaser.GameObjects.Text
+	private score: number = 0
+
 	private pineapple?: Phaser.Physics.Arcade.Sprite
 	private mango?: Phaser.Physics.Arcade.Sprite
     private avocado?: Phaser.Physics.Arcade.Sprite
-	private currentFruit?: Phaser.Physics.Arcade.Sprite
+	private fruit?: Phaser.Physics.Arcade.Image
+	private currentFruitName?: String
 
 	private baskets?: Phaser.Physics.Arcade.StaticGroup
-	private ifNative?: Phaser.GameObjects.Text
-	private ifNotNative?: Phaser.GameObjects.Text
-	private ifGreen?: Phaser.GameObjects.Text
-	private ifNotGreen?: Phaser.GameObjects.Text
+	private ifBerry?: Phaser.GameObjects.Text
+	private ifNotBerry?: Phaser.GameObjects.Text
+	private ifOrange?: Phaser.GameObjects.Text
+	private ifNotOrange?: Phaser.GameObjects.Text
+
+	private finishText?: Phaser.GameObjects.Text
+
+	private backButton?: Phaser.GameObjects.Image;
+
+	private fruitIntro?: Phaser.GameObjects.Image;
+	private closeButton?: Phaser.GameObjects.Image;
+	private aboutButton?: Phaser.GameObjects.Image;
+	private openInfo?: Phaser.GameObjects.Image;
+	private infoBox: boolean = true;
 
 	constructor() {
 		super('level-2')
 	}
 
 	preload() {
-		this.load.image("pineapple", "assets/big_pineapple.png");
-		this.load.image("mango", "assets/big_mango.png");
-        this.load.image("avocado", "assets/big_avocado.png");
-		this.load.image("basket", "assets/big_basket.png");
+		this.load.image("pineapple", "assets/pineapple.png");
+		this.load.image("mango", "assets/mango.png");
+        this.load.image("avocado", "assets/avocado.png");
+		this.load.image("lychee", "assets/lychee.png");
+		this.load.image("papaya", "assets/papaya.png");
+        this.load.image("passionfruit", "assets/passionfruit.png");
+		this.load.image("basket", "assets/basket.png");
 		this.load.image("factory", "assets/factory.png");
 		this.load.image("vertical", "assets/vertical_conveyor.png")
 		this.load.image("horizontal", "assets/horizontal_conveyor.png")
+		this.load.image("l-arrow", "public/assets/Arrow Left.png");
+		this.load.image("r-arrow", "public/assets/Arrow Right.png");
+		this.load.image('modal-bg', 'assets/modal.png');
+		this.load.image('back-button', 'public/assets/Back.png');
+		this.load.image('close-button', 'public/assets/close.png');
+		this.load.image('fruit-intro', 'assets/emptyDescription.png');
+		this.load.image('about-button', 'assets/aboutButton.png');
 	}
 
 	create() {
+		this.infoBox = true;
+
 		this.add.image(400, 300, "factory")
 
-		this.add.tileSprite(450, 100, 60, 250, "vertical");
-		this.add.tileSprite(350, 250, 500, 60, "horizontal");
-		this.add.tileSprite(600, 475, 60, 500, "vertical");
-		this.add.tileSprite(70, 475, 60, 500, "vertical")
-		this.add.tileSprite(260, 475, 60, 500, "vertical")
+		this.conv_vertical1 = this.add.tileSprite(450, 100, 60, 250, "vertical");
+		this.conv_horizontal1 = this.add.tileSprite(450, 250, 450, 60, "horizontal");
+		this.conv_vertical2 = this.add.tileSprite(650, 475, 60, 500, "vertical");
+		this.conv_vertical3 = this.add.tileSprite(250, 350, 60, 250, "vertical");
+		this.conv_horizontal2 = this.add.tileSprite(250, 450, 350, 60, "horizontal");
+		this.conv_vertical4 = this.add.tileSprite(100, 525, 60, 200, "vertical");
+		this.conv_vertical5 = this.add.tileSprite(400, 525, 60, 200, "vertical");
+
+		this.cursors = this.input.keyboard.createCursorKeys()
+		this.arrow1 = this.add.image(450, 300,'r-arrow');
+		this.arrow2 = this.add.image(250, 500,'r-arrow')
 
 		this.baskets = this.physics.add.staticGroup()
-		this.baskets.create(70, 550, "basket")
-		this.baskets.create(260, 550, "basket")
-		this.baskets.create(600, 550, "basket")
+		this.baskets.create(100, 550, "basket")
+		this.baskets.create(400, 550, "basket")
+		this.baskets.create(650, 550, "basket")
 
-		this.ifGreen= this.add.text(165, 200, 'Is 🟢', {
+		this.ifOrange = this.add.text(165, 390, 'Is 🟠', {
 			fontSize: '14px',
 			color: 'black',
 			backgroundColor: 'white'
 		}).setOrigin(0.5)
 
-		this.ifNotGreen= this.add.text(355, 390, 'Not 🟢', {
+		this.ifNotOrange= this.add.text(355, 390, 'Not 🟠', {
 			fontSize: '14px',
 			color: 'black',
 			backgroundColor: 'white'
 		}).setOrigin(0.5)
 
-		this.ifNative = this.add.text(355, 200, 'Native', {
+		this.ifBerry = this.add.text(355, 200, 'Berry', {
 			fontSize: '14px',
 			color: 'black',
 			backgroundColor: 'white'
 		}).setOrigin(0.5)
 
-		this.ifNotNative = this.add.text(555, 200, 'Not Native', {
+		this.ifNotBerry = this.add.text(555, 200, 'Not a Berry', {
 			fontSize: '14px',
 			color: 'black',
 			backgroundColor: 'white'
 		}).setOrigin(0.5)
 
-		this.currentFruit = this.physics.add.sprite(450, 150, "pineapple")
+		this.fruit = this.physics.add.image(450, 150, "pineapple")
+
+		this.scoreText = this.add.text(700, 30, 'Score: '+this.score, {
+			fontSize: '25px',
+			color: 'black',
+			backgroundColor: 'white'
+		}).setOrigin(0.5);
+
+		this.backButton = this.add.image(50,40, 'back-button').setAlpha(1);
+        this.backButton.setScale(.25)
+        this.backButton.setInteractive();
+        this.backButton.on("pointerover",() =>{
+            this.backButton?.setAlpha(.5);
+        });
+        this.backButton.on("pointerout", ()=>{
+            this.backButton?.setAlpha(1);
+        });
+        this.backButton.on("pointerup",()=>{
+            this.scene.stop('level-1');
+            this.scene.start('MapScene');
+        });
+		this.fruitIntro = this.add.image(400,300,'fruit-intro')
+		this.infoBox = true;
+		this.closeButton = this.add.image(400,470, 'close-button').setAlpha(1);
+        this.closeButton.setScale(.08)
+        this.closeButton.setInteractive();
+        this.closeButton.on("pointerover",() =>{
+            this.closeButton?.setAlpha(.5);
+        });
+        this.closeButton.on("pointerout", ()=>{
+            this.closeButton?.setAlpha(1);
+        });
+        this.closeButton.on("pointerup",()=>{
+			if(this.fruitIntro && this.closeButton){
+            	this.fruitIntro.visible = false
+				this.closeButton.visible = false
+			}
+			this.infoBox = false
+        });
+
+		this.speedText = this.add.text(700, 70, 'Speed: '+this.speed, {
+			fontSize: '25px',
+			color: 'black',
+			backgroundColor: 'blue'
+		}).setOrigin(0.5)
+		this.speedText.setInteractive();
+        this.speedText.on("pointerover",() =>{
+            this.speedText?.setAlpha(.5);
+        });
+        this.speedText.on("pointerout", ()=>{
+            this.speedText?.setAlpha(1);
+        });
+        this.speedText.on("pointerup",()=>{
+			if(this.speed >= 4){
+				this.speed = 1
+			} else {
+            	this.speed += 1
+			}
+			this.speedText?.setText('Speed: '+this.speed)
+        });
+		this.aboutButton = this.add.image(150,40, 'about-button').setAlpha(1);
+        this.aboutButton.setScale(.3)
+        this.aboutButton.setInteractive();
+        this.aboutButton.on("pointerover",() =>{
+            this.aboutButton?.setAlpha(.5);
+        });
+        this.aboutButton.on("pointerout", ()=>{
+            this.aboutButton?.setAlpha(1);
+        });
+        this.aboutButton.on("pointerup",()=>{
+            if(this.fruitIntro && this.closeButton){
+            	this.fruitIntro.visible = true
+				this.closeButton.visible = true
+			}
+			this.infoBox = true
+        });
 	}
 
 	update() {
-		var pineapple = this.input.keyboard.addKey('P');
-		var mango = this.input.keyboard.addKey('M');
-		var avocado = this.input.keyboard.addKey('A');
-		if(pineapple.isDown){
-			this.currentFruit?.setTexture("pineapple")
+		if(this.infoBox){
+			return
 		}
-		if(mango.isDown){
-			this.currentFruit?.setTexture("mango")
+		if(!this.cursors || !this.conv_vertical1 || !this.conv_vertical2 || !this.conv_vertical3 
+			|| !this.conv_vertical4 || !this.conv_vertical5 ||
+			!this.conv_horizontal1 || !this.conv_horizontal2 || !this.fruit){
+			return
 		}
-		if(avocado.isDown){
-			this.currentFruit?.setTexture("avocado")
+		if(this.cursors?.left.isDown){
+			this.arrow1?.setTexture('l-arrow');
+			this.arrow2?.setTexture('l-arrow');
+			this.currDirection = 1;
+		}
+		if(this.cursors?.right.isDown){
+			this.arrow1?.setTexture('r-arrow');
+			this.arrow2?.setTexture('r-arrow');
+			this.currDirection = 0;
+		}
+		this.conv_vertical1.tilePositionY -= this.speed;
+		this.conv_vertical2.tilePositionY -= this.speed;
+		this.conv_vertical3.tilePositionY -= this.speed;
+		this.conv_vertical4.tilePositionY -= this.speed;
+		this.conv_vertical5.tilePositionY -= this.speed;
+		if(this.currDirection === 0){
+			this.conv_horizontal1.tilePositionX -= this.speed;
+			this.conv_horizontal2.tilePositionX -= this.speed;
+		} else {
+			this.conv_horizontal1.tilePositionX += this.speed;
+			this.conv_horizontal2.tilePositionX += this.speed;
+		}
+
+		if(this.fruit.y < 250 || this.fruit.x == 650 || 
+			(this.fruit.x == 100 && this.fruit.y >= 450) || 
+			(this.fruit.x == 250 && this.fruit.y < 450) || 
+			(this.fruit.x == 400 && this.fruit.y >= 450)){
+			this.fruit.y += this.speed;
+		} else {
+			if(this.currDirection === 0){
+				this.fruit.x += this.speed;
+			} else {
+				this.fruit.x -= this.speed;
+			}
+		}
+
+		//Prevent fruit From flying off first horizontal conveyor belt
+		if(this.fruit.y >= 250 && this.fruit.y < 450){
+			if(this.fruit.x < 250){
+				this.fruit.x = 250;
+			}
+			else if(this.fruit.x > 650){
+				this.fruit.x = 650;
+			}
+		}
+
+		//Prevent fruit From flying off second horizontal conveyor belt
+		if(this.fruit.y >= 450){
+			if(this.fruit.x < 100){
+				this.fruit.x = 100;
+			}
+			else if(this.fruit.x > 400){
+				this.fruit.x = 400;
+			}
+		}
+	}
+
+	newFruit(){
+		let newFruitIndex = Math.floor(Math.random() * 4)
+		if(newFruitIndex == 0){
+			this.currentFruitName = "pineapple"
+			this.fruit?.setTexture("pineapple")
+		}
+		else if(newFruitIndex == 1){
+			this.currentFruitName = "mango"
+			this.fruit?.setTexture("mango")
+		}
+		else if(newFruitIndex == 2){
+			this.currentFruitName = "lychee"
+			this.fruit?.setTexture("lychee")
+		}
+		else{
+			this.currentFruitName = "avocado"
+			this.fruit?.setTexture("avocado")
 		}
 	}
 }
